f11f6365d844bcc0f38f5a1fff7262dcd6d7c01b
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-08-26 10:38:10 -0700

    added implementations for Parser

--- nand2tetris/projects/06/Parser.cpp
+++ nand2tetris/projects/06/Parser.cpp
@@ -17,7 +17,11 @@ std::string Parser::comp() const {
 }
 
 std::string Parser::dest() const {
-    return std::__cxx11::string();
+    size_t idx = current_instruction.find('=');
+    if(idx == std::string::npos){
+        return "null";
+    }
+    return current_instruction.substr(0, idx);
 }
 
 Parser::Parser(const std::string &filename) : file_stream(filename)
