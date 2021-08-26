4e6d902004cb9d6b1bc5986f39e3a8081fa8b4ef
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-08-26 12:16:01 -0700

    partial implementation

--- nand2tetris/projects/06/main.cpp
+++ nand2tetris/projects/06/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Parser.h"
 #include "Code.h"
 
@@ -7,11 +8,46 @@ bool getLine(std::string& line){
     return !line.empty();
 }
 
-int main() {
+void initialize_symbol_table(std::unordered_map<std::string, int>& symbol_table){
+    for(int i = 0;i <= 15;i++){
+        std::string symbol{"R" + std::to_string(i)};
+        symbol_table.insert({symbol, i});
+    }
+    symbol_table.insert({"SCREEN", 16384});
+    symbol_table.insert({"KBD", 24576});
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2){
+        std::cerr << "usage: ./out file.asm" << std::endl;
+    }
+    Parser parser(argv[1]);
+    std::unordered_map<std::string, int> symbol_table;
+    initialize_symbol_table(symbol_table);
+    // first_pass
+    int line_number = 0;
+    while(parser.hasMoreLines()){
+        parser.advance();
+        if(parser.currentInstructionType() == instructionType::A_INSTRUCTION ||
+           parser.currentInstructionType() == instructionType::C_INSTRUCTION){
+            line_number++;
+            continue;
+        } else if(parser.currentInstructionType() == instructionType::L_INSTRUCTION){
+            std::string loop_symbol{parser.symbol()};
+            symbol_table.insert({loop_symbol, line_number + 1});
+        } // the instruction is a pseudo-instruction. ignore for now
+    }
+    // second pass
+    int n = 16;
+    parser = Parser{argv[1]};
     Code code;
-    std::string userInput;
-    while(getLine(userInput)){
-        std::string c = code.comp(userInput);
-        std::cout << c << std::endl;
+    while(parser.hasMoreLines()){
+        parser.advance();
+        if(parser.currentInstructionType() == instructionType::C_INSTRUCTION){
+            std::cout << parser.currentInstruction() << std::endl;
+            std::cout << "comp: " << parser.comp() << std::endl;
+            std::cout << "dest: " << parser.dest() << std::endl;
+            std::cout << "jump: " << parser.jump() << std::endl;
+        }
     }
 }
