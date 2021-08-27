2bc409da86e6be2217abb274815ffb8e301bc7bb
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-08-27 15:16:58 -0700

    added implementation of parser functions.

--- nand2tetris/projects/07/Parser.cpp
+++ nand2tetris/projects/07/Parser.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Parser.h"
+
+bool Parser::hasMoreLines() const {
+    return !file_stream.eof();
+}
+
+std::string Parser::currentInstruction() const {
+    return current_instruction;
+}
+
+INSTRUCTION_TYPE Parser::currentInstructionType() const {
+    return current_instruction_type;
+}
+
+int Parser::arg2() const {
+    return 0;
+}
+
+void Parser::advance() {
+
+}
+
+std::string Parser::arg1() const {
+    return std::__cxx11::string();
+}
--- nand2tetris/projects/07/Parser.h
+++ nand2tetris/projects/07/Parser.h
@@ -5,9 +5,25 @@
 #ifndef INC_07_PARSER_H
 #define INC_07_PARSER_H
 
+#include <string>
+#include <fstream>
+
+enum class INSTRUCTION_TYPE {C_ARITHMETIC, C_PUSH, C_POP, C_LABEL,
+        C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL, C_PSEUDO};
 
 class Parser {
-
+public:
+    explicit Parser(const std::string& filename);
+    bool hasMoreLines() const;
+    void advance();
+    std::string arg1() const;
+    int arg2() const;
+    INSTRUCTION_TYPE currentInstructionType() const;
+    std::string currentInstruction() const;
+private:
+    INSTRUCTION_TYPE current_instruction_type;
+    std::string current_instruction;
+    std::fstream file_stream;
 };
 
 
