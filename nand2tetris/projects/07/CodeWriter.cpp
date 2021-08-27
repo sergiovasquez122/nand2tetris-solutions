d513ddd7325cd111a69ceee95534f4857461db95
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-08-27 16:48:04 -0700

    added function definitions for CodeWriter.

--- nand2tetris/projects/07/CodeWriter.cpp
+++ nand2tetris/projects/07/CodeWriter.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "CodeWriter.h"
+
+CodeWriter::CodeWriter(const std::string &filename) {
+
+}
+
+void CodeWriter::writeArithmetic(const std::string& command) {
+
+}
+
+void CodeWriter::writePush(const std::string &segment, int index) {
+
+}
+
+void CodeWriter::writePop(const std::string &segment, int index) {
+
+}
+
+void CodeWriter::close() {
+
+}
--- nand2tetris/projects/07/CodeWriter.h
+++ nand2tetris/projects/07/CodeWriter.h
@@ -5,9 +5,15 @@
 #ifndef INC_07_CODEWRITER_H
 #define INC_07_CODEWRITER_H
 
+#include <string>
 
 class CodeWriter {
-
+public:
+    explicit CodeWriter(const std::string& filename);
+    void writeArithmetic(const std::string& command);
+    void writePush(const std::string& segment, int index);
+    void writePop(const std::string& segment, int index);
+    void close();
 };
 
 
