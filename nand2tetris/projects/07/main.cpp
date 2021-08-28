8bfa3faa51d6d5015e55a7ba95ca5ee97da41943
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2021-08-28 10:03:46 -0700

    implementation of main.

--- nand2tetris/projects/07/main.cpp
+++ nand2tetris/projects/07/main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include "Parser.h"
+#include "CodeWriter.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "usage: ./out file.vm" << std::endl;
     }
     Parser parser(argv[1]);
-
+    CodeWriter writer(argv[1]);
     while (parser.hasMoreLines()) {
         parser.advance();
         if(parser.currentInstructionType() == INSTRUCTION_TYPE::C_PSEUDO){
             continue;
         }
-        std::cout << "[current instruction:] " << parser.currentInstruction() << " [current instruction type:] " << parser.currentInstructionType() << std::endl;
         if(parser.currentInstructionType() == INSTRUCTION_TYPE::C_PUSH){
             std::string first_arg = parser.arg1();
             int second_arg = parser.arg2();
-            std::cout << "first arg: " << first_arg << " second_arg: " << second_arg << std::endl;
-            std::cout << "first arg size: " << first_arg.size() << std::endl;
+            writer.writePush(first_arg, second_arg);
         } else if(parser.currentInstructionType() == INSTRUCTION_TYPE::C_POP){
             std::string first_arg = parser.arg1();
             int second_arg = parser.arg2();
-            std::cout << "first arg: " << first_arg << " second_arg: " << second_arg << std::endl;
-            std::cout << "first arg size: " << first_arg.size() << std::endl;
+            writer.writePop(first_arg, second_arg);
         } else if(parser.currentInstructionType() == INSTRUCTION_TYPE::C_ARITHMETIC){
             std::string first_arg = parser.arg1();
-            std::cout << "first arg: " << first_arg << std::endl;
+            writer.writeArithmetic(first_arg);
         }
     }
 }
